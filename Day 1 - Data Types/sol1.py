fbfb2bb463166db55f937539a0309ff75a214daf
Apollyion <lucas.jlb19@gmail.com>
2022-01-13 14:17:05 -0300

    Codigos do Dia 0 ao Dia 6 - Apenas Adicionei

---
+++ Day 1 - Data Types/sol1.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+inteiro = 1
+double = 5.5
+string = "Aoba"
+# Read and save an integer, double, and String to your variables.
+inteiro = int(input())
+db = float(input())
+string = input()
+# Print the sum of both integer variables on a new line.
+print(i + inteiro)
+# Print the sum of the double variables on a new line.
+print(d + db)
+# Concatenate and print the String variables on a new line
+z = s + string
+print(z)
+# The 's' variable above should be printed first.
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# 30daysOfCode-Writeup
+# [ Hacker Rank - 30 Days of Code - Write Up](https://www.hackerrank.com/domains/tutorials/30-days-of-code)
+- - -
+
+ - This is my Python solution for the 30 Days of Code challenge from Hacker Rank, it has some janky code but it's all about learning!
+
+- The solutions are divided by day and also include the tutorial suggested by the site.
+
+- - -
+
+My Hacker Rank Account:
+@lucas_jlb19
